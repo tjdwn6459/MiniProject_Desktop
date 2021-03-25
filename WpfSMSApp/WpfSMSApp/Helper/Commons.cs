1517b7595bfe670cb57d47cbf2d392ca7b070004
tjdwn6459 <tjdwn6459@naver.com>
2021-03-25 17:20:09 +0900

    210325 오늘의 커밋

---
+++ WpfSMSApp/WpfSMSApp/Helper/Commons.cs
@@ -0,0 +1,15 @@
+﻿using NLog;
+using WpfSMSApp.Model;
+
+namespace WpfSMSApp
+{
+    public class Commons
+    {
+        //NLOg 정적 인스턴스 생성
+        public static readonly Logger LOGGER = LogManager.GetCurrentClassLogger();
+
+        //로그인한 유저 정보
+        public static User LOGINED_USER;
+
+    }
+}
